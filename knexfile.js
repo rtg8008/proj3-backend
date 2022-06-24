c81ee7697ce405b765339e8e84e98037a2f964a1
Ryan Guinter <421guinterr@gmail.com>
2022-06-23 19:45:02 -0600

    something

--- knexfile.js
+++ knexfile.js
@@ -5,15 +5,29 @@
  */
 module.exports = {
 
+  // development: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     host: 'db',
+  //     password: 'docker',
+  //     user: 'postgres', //default postgres user
+  //     port: 5432,
+  //     database: 'weg_database' // expected database name to be created
+  //   }
+  // },
   development: {
-    client: 'postgresql',
+    client: "pg",
     connection: {
-      host: 'db',
-      password: 'docker',
-      user: 'postgres', //default postgres user
-      port: 5432,
-      database: 'weg_database' // expected database name to be created
-    }
+      connectionString: process.env.DATABASE_URL || `postgres://hagivgcqjgmsbg:1bd8e665245f4e4f2c9ee5fc63c2b5b0998b39a1d29ac8c956db6a1fdde6a6c3@ec2-44-197-128-108.compute-1.amazonaws.com:5432/d65nmhbo7htq0s`
+      ,
+      ssl: { rejectUnauthorized: false },
+    },
+    migrations: {
+      directory: "./migrations",
+    },
+    seeds: {
+      directory: "./seeds",
+    },
   },
   test: {
     client: 'postgresql',
